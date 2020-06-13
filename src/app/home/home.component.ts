b08201f12e92773e89dba5a5485899d003baf645
AsZALenge <sathapornsurak@gmail.com>
2020-06-13 17:40:22 +0700

    Edit home

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,8 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-  proJect = 'อะไรก้อได้';
-  nameTh = 'TEST';
+  proJect = 'AsZALenge';
+  nameTh1 = 'Ch';
+  nameTh2 = 'Ch';
   constructor() { }
 
   ngOnInit() {
